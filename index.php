f638a5f18f1c7794f1ab4af9a9d723bfbc881c5a
Echelebo <echelebochibuike@gmail.com>
2025-02-16 18:42:30 +0100

    first

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
           <div class="carousel owl-carousel custom-carousel carousel-navs carousel-dots" data-slide="1" data-slide-rs="1" data-autoplay="false" data-nav="true" data-dots="true" data-space="0" data-loop="true" data-speed="800" data-slider-id="#custom-carousel">
             <!--  Start .slide-->
             <div class="slide d-flex align-items-center bg-overlay bg-overlay-dark">
-              <div class="bg-section"><img src="assets/images/sliders/5.jpg" alt="Background"/></div>
+              <div class="bg-section"><img src="assets/images/sliders/4.jpg" alt="Background"/></div>
               <div class="container">
                 <div class="row">
                   <div class="col-12 col-lg-7">
@@ -20,7 +20,7 @@
                         <li><i class="fas fa-check-circle"></i> Highly Professional Staff, Accurate Testing Processes</li>
                         <li><i class="fas fa-check-circle"></i> Unrivalled workmanship, Professional and Qualified</li>
                       </ul>
-                      <div class="slide-buttons"><a class="btn btn--white btn--inverse" href="javascript:void(0)">more about us</a><a class="btn btn--transparent" href="javascript:void(0)">our services</a></div>
+                      <div class="slide-buttons"><a class="btn btn--white btn--inverse" href="page-about.php">more about us</a><a class="btn btn--transparent" href="contact.php">Contact Us</a></div>
                     </div>
                     <!-- End .slide-content -->
                   </div>
@@ -42,7 +42,7 @@
                         <li><i class="fas fa-check-circle"></i> Highly Professional Staff, Accurate Testing Processes</li>
                         <li><i class="fas fa-check-circle"></i> Unrivalled workmanship, Professional and Qualified</li>
                       </ul>
-                      <div class="slide-buttons"><a class="btn btn--white btn--inverse" href="javascript:void(0)">more about us</a><a class="btn btn--transparent" href="javascript:void(0)">our services</a></div>
+                      <div class="slide-buttons"><a class="btn btn--white btn--inverse" href="page-about.php">more about us</a><a class="btn btn--transparent" href="contact.php">Contact Us</a></div>
                     </div>
                     <!-- End .slide-content -->
                   </div>
@@ -53,7 +53,7 @@
             <!-- End .slide-->
             <!--  Start .slide-->
             <div class="slide d-flex align-items-center bg-overlay bg-overlay-dark">
-              <div class="bg-section"><img src="assets/images/sliders/4.jpg" alt="Background"/></div>
+              <div class="bg-section"><img src="assets/images/sliders/5.jpg" alt="Background"/></div>
               <div class="container">
                 <div class="row">
                   <div class="col-12 col-lg-7">
@@ -61,7 +61,7 @@
                       <p class="slide-subheadline">Flexibile, Improved And accelerated Solutions</p>
                       <h2 class="slide-headline">your flexible<br/>road directions!</h2>
                       <p class="slide-desc">Representative logistics operator providing full range of service in the sphere of customs clearance and transportation worldwide.</p>
-                      <div class="slide-buttons"><a class="btn btn--primary" href="javascript:void(0)">more about us</a><a class="btn btn--white" href="javascript:void(0)">our services</a></div>
+                      <div class="slide-buttons"><a class="btn btn--primary" href="page-about.php">more about us</a><a class="btn btn--white" href="contact.php">Contact Us</a></div>
                     </div>
                     <!-- End .slide-content -->
                   </div>
@@ -225,7 +225,7 @@
                         <li> <a><i class="fas fa-check-circle"></i><span>highly professional staff</span></a></li>
                       </ul>
                     </div>
-                    <div class="service-more"><a class="btn" href="javascript:void(0)"> <i class="icon-arrow-right"></i><span>read more</span></a></div>
+                    <div class="service-more"><a class="btn" href="service-air-freight.php"> <i class="icon-arrow-right"></i><span>read more</span></a></div>
                   </div>
                   <!-- End .work-content -->
                 </div>
@@ -251,7 +251,7 @@
                         <li> <a><i class="fas fa-check-circle"></i><span>professional &amp; qualified</span></a></li>
                       </ul>
                     </div>
-                    <div class="service-more"><a class="btn" href="javascript:void(0)"> <i class="icon-arrow-right"></i><span>read more</span></a></div>
+                    <div class="service-more"><a class="btn" href="service-ocean-freight.php"> <i class="icon-arrow-right"></i><span>read more</span></a></div>
                   </div>
                   <!-- End .work-content -->
                 </div>
@@ -277,7 +277,7 @@
                         <li> <a><i class="fas fa-check-circle"></i><span>logistical expertise</span></a></li>
                       </ul>
                     </div>
-                    <div class="service-more"><a class="btn" href="javascript:void(0)"> <i class="icon-arrow-right"></i><span>read more</span></a></div>
+                    <div class="service-more"><a class="btn" href="service-road-freight.php"> <i class="icon-arrow-right"></i><span>read more</span></a></div>
                   </div>
                   <!-- End .work-content -->
                 </div>
@@ -319,151 +319,7 @@
               </div>
             </div>
             <!--End .col-lg-6-->
-            <div class="col-12">
-              <div class="contact-panel contact-panel-2">
-                <div class="contact-types"> <a class="button quote-btn" href="javascript:void(0)" data-form="quoteFormActive"> <i class="flaticon-020-order"> </i><span>request a quote</span></a><a class="button track-btn active" href="javascript:void(0)" data-form="trackFormActive"> <i class="flaticon-009-shipping-1"> </i><span>track &amp; trace</span></a></div>
-                <div class="contact-card">
-                  <div class="contact-body trackFormActive">
-                    <div class="row">
-                      <div class="col-12 col-lg-8">
-                        <form class="contactForm quote-form mb-0" method="post" action="https://demo.zytheme.com/Elexpress Freight/assets/php/contact.php">
-                          <h5 class="card-heading">personal data</h5>
-                          <div class="row">
-                            <div class="col-12 col-md-12 col-lg-4">
-                              <input class="form-control" type="text" name="contact-name" placeholder="Name" required=""/>
-                            </div>
-                            <div class="col-12 col-md-6 col-lg-4">
-                              <input class="form-control" type="text" name="contact-email" placeholder="Email" required=""/>
-                            </div>
-                            <div class="col-12 col-md-6 col-lg-4">
-                              <input class="form-control" type="text" name="contact-phone" placeholder="Phone" required=""/>
-                            </div>
-                          </div>
-                          <h5 class="card-heading">shipment data</h5>
-                          <div class="row">
-                            <div class="col-12 col-lg-4">
-                              <div class="select-container">
-                                <select class="form-control">
-                                  <option value="default">freight type</option>
-                                  <option value="AL">freight type 1</option>
-                                  <option value="AK">freight type 2</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div class="col-12 col-md-6 col-lg-4">
-                              <input class="form-control" type="text" name="contact-city" placeholder="city of departure" required=""/>
-                            </div>
-                            <div class="col-12 col-md-6 col-lg-4">
-                              <input class="form-control" type="text" name="contact-destination" placeholder="delivery city" required=""/>
-                            </div>
-                            <div class="col-12 col-lg-4">
-                              <div class="select-container">
-                                <select class="form-control">
-                                  <option value="default">incoterms</option>
-                                  <option value="AL">incoterms 1</option>
-                                  <option value="AK">incoterms 2</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div class="col-12 col-md-2">
-                              <input class="form-control" type="text" name="contact-weight" placeholder="weight" required=""/>
-                            </div>
-                            <div class="col-12 col-md-2">
-                              <input class="form-control" type="text" name="contact-height" placeholder="height" required=""/>
-                            </div>
-                            <div class="col-12 col-md-2">
-                              <input class="form-control" type="text" name="contact-width" placeholder="width" required=""/>
-                            </div>
-                            <div class="col-12 col-md-2">
-                              <input class="form-control" type="text" name="contact-length" placeholder="length" required=""/>
-                            </div>
-                            <div class="col-12">
-                              <div class="custom-radio-group">
-                                <div class="custom-control custom-radio custom-control-inline">
-                                  <input class="custom-control-input" type="radio" id="customRadioInline1" name="customRadioInline1"/>
-                                  <label for="customRadioInline1">fragile</label>
-                                </div>
-                                <div class="custom-control custom-radio custom-control-inline">
-                                  <input class="custom-control-input" type="radio" id="customRadioInline2" name="customRadioInline1"/>
-                                  <label for="customRadioInline2">express delivery</label>
-                                </div>
-                                <div class="custom-control custom-radio custom-control-inline">
-                                  <input class="custom-control-input" type="radio" id="customRadioInline3" name="customRadioInline1"/>
-                                  <label for="customRadioInline3">insurance</label>
-                                </div>
-                                <div class="custom-control custom-radio custom-control-inline">
-                                  <input class="custom-control-input" type="radio" id="customRadioInline4" name="customRadioInline1"/>
-                                  <label for="customRadioInline4">packaging</label>
-                                </div>
-                              </div>
-                            </div>
-                            <div class="col-12">
-                              <input class="btn btn--secondary btn--block" type="submit" value="Request A Quote"/>
-                            </div>
-                            <div class="col-12">
-                              <div class="contact-result"></div>
-                            </div>
-                          </div>
-                        </form>
-                        <form class="contactForm track-form mb-0" method="post" action="https://demo.zytheme.com/Elexpress Freight/assets/php/contact.php">
-                          <h5 class="card-heading">personal data</h5>
-                          <div class="row">
-                            <div class="col-12">
-                              <div class="select-container">
-                                <select class="form-control">
-                                  <option value="default">shipment type</option>
-                                  <option value="AL">type 1</option>
-                                  <option value="AK">type 2</option>
-                                </select>
-                              </div>
-                            </div>
-                          </div>
-                          <h5 class="card-heading">tracking number</h5>
-                          <div class="row">
-                            <div class="col-12">
-                              <textarea class="form-control" name="contact-tracking-number" cols="30" rows="2" placeholder="You can enter up to a maximum of 10 airway bill numbers for tracking." required=""></textarea>
-                            </div>
-                            <div class="col-12">
-                              <div class="custom-radio-group">
-                                <div class="custom-control custom-radio custom-control-inline">
-                                  <input class="custom-control-input" type="radio" id="customRadioInline5" name="customRadioInline1"/>
-                                  <label for="customRadioInline5">fragile</label>
-                                </div>
-                                <div class="custom-control custom-radio custom-control-inline">
-                                  <input class="custom-control-input" type="radio" id="customRadioInline6" name="customRadioInline1"/>
-                                  <label for="customRadioInline6">express delivery</label>
-                                </div>
-                                <div class="custom-control custom-radio custom-control-inline">
-                                  <input class="custom-control-input" type="radio" id="customRadioInline7" name="customRadioInline1"/>
-                                  <label for="customRadioInline7">insurance</label>
-                                </div>
-                                <div class="custom-control custom-radio custom-control-inline">
-                                  <input class="custom-control-input" type="radio" id="customRadioInline8" name="customRadioInline1"/>
-                                  <label for="customRadioInline8">packaging</label>
-                                </div>
-                              </div>
-                            </div>
-                            <div class="col-12">
-                              <input class="btn btn--secondary btn--block" type="submit" value="Track &amp; Trace"/>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
-                      <div class="col-12 col-lg-4">
-                        <div class="apply-card widget widget-reservation"><img src="assets/images/contact/1.jpg" alt="image"/>
-                          <div class="widget-content">
-                            <h5>take your carreer to next level</h5>
-                            <p>We pride ourselves on providing the best transport & shipping services available all over the world and there is no one comes close to us. </p><a class="btn btn--white" href="javascript:void(0)"><i class="icon-arrow-right"></i>apply now!</a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- End .contact-body -->
-                </div>
-              </div>
-              <!-- End .contact-panel-->
-            </div>
+
             <!-- End .col-lg-6-->
           </div>
           <!-- End .row-->
@@ -524,7 +380,7 @@
           <div class="col-12 col-lg-6">
             <div class="video video-1" id="video1">
               <div class="bg-section"><img src="assets/images/video/4.jpg" alt="background"/></div>
-              <div class="player"><a class="popup-video" href="https://www.youtube.com/watch?v=7e90gBu4pas"> <i class="fas fa-play"></i></a></div>
+              <div class="player"><a class="popup-video" href="javascript:void(0)"> </a></div>
             </div>
           </div>
         </div>
@@ -537,4 +393,4 @@
       -->
 
 
-<?php include_once "footer.php"; ?>
+<?php include_once "footer.php"; ?>d
